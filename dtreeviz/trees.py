9ead130c7018364951f422126de8e5123e73945e
parrt <parrt@cs.usfca.edu>
2018-11-13 14:35:52 -0800

    rm rotate on rectangles.

--- dtreeviz/trees.py
+++ dtreeviz/trees.py
@@ -188,14 +188,13 @@ def rtreeviz_bivar_heatmap(ax, X_train, y_train, max_depth, features, feature_na
     tesselation = shadow_tree.tesselation()
 
     for node,bbox in tesselation:
-        #node,bbox = tess
+        pred = node.prediction()
+        color = color_map[int(((pred - y_lim[0]) / y_range) * 99)]
         x = bbox[0]
         y = bbox[1]
         w = bbox[2]-bbox[0]
         h = bbox[3]-bbox[1]
-        pred = node.prediction()
-        color = color_map[int(((pred - y_lim[0]) / y_range) * 99)]
-        rect = patches.Rectangle((x, y), w, h, .08, linewidth=.3, alpha=.5,
+        rect = patches.Rectangle((x, y), w, h, 0, linewidth=.3, alpha=.5,
                                  edgecolor=GREY, facecolor=color)
         ax.add_patch(rect)
 
@@ -404,7 +403,7 @@ def ctreeviz_bivar(ax, X_train, y_train, max_depth, features, feature_names, cla
         y = bbox[1]
         w = bbox[2]-bbox[0]
         h = bbox[3]-bbox[1]
-        rect = patches.Rectangle((x, y), w, h, .08, linewidth=.3, alpha=.4,
+        rect = patches.Rectangle((x, y), w, h, 0, linewidth=.3, alpha=.4,
                                  edgecolor=GREY, facecolor=colors[node.prediction()])
         ax.add_patch(rect)
 
--- testing/play_rtree_bivar_heatmap.py
+++ testing/play_rtree_bivar_heatmap.py
@@ -9,16 +9,15 @@ df_cars = pd.read_csv("data/cars.csv")
 X = df_cars.drop('MPG', axis=1)
 y = df_cars['MPG']
 
-max_depth = 3
-figsize = (6,5)
+max_depth = 2
+figsize = (6, 5)
 fig, ax = plt.subplots(1, 1, figsize=figsize)
-#ax = fig.add_subplot(111, projection='3d')
 t = rtreeviz_bivar_heatmap(ax,
-                   X,y,
-                   max_depth=max_depth,
-                   features=[2,1],
-                   feature_names=['Vehicle Weight', 'Horse Power'],
-                   target_name='MPG',
-                   fontsize=14)
+                           X, y,
+                           max_depth=max_depth,
+                           features=[2, 1],
+                           feature_names=['Vehicle Weight', 'Horse Power'],
+                           target_name='MPG',
+                           fontsize=14)
 plt.savefig(f"/tmp/rtree-depth-{max_depth}.svg", bbox_inches=0, pad_inches=0)
 plt.show()
